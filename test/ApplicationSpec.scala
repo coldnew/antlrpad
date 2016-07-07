8680bbbde31b139108380f24e75da6fa80aa4618
alexey.nesterov <alexey.nesterov@totalobjects.co.uk>
2016-07-07 11:47:52 +0100

    Add index page test

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,6 +1,7 @@
 import org.scalatestplus.play._
 import play.api.test._
 import play.api.test.Helpers._
+import akka.stream.{Materializer}
 
 /**
  * Add your spec here.
@@ -9,10 +10,23 @@ import play.api.test.Helpers._
  */
 class ApplicationSpec extends PlaySpec with OneAppPerTest {
 
+  implicit lazy val materializer: Materializer = app.materializer
+
   "Routes" should {
 
     "send 404 on a bad request" in  {
       route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
+    }
+
+  }
+
+  "Index page " should {
+
+    "give static page " in {
+      val home = route(app, FakeRequest(GET, "/")).get
+
+      status(home) mustBe OK
+      contentAsString(home) must include("<title>AntlrPad</title>")
     }
 
   }
