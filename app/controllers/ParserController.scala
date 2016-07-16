ef6ffeab5fac9916aae511d38ee6e8a8c139ba83
Alexey Nesterov <alexey.nesterov@live.com>
2016-07-16 13:59:46 +0100

    Return form errors as JSON

--- app/controllers/ParserController.scala
+++ app/controllers/ParserController.scala
@@ -4,12 +4,13 @@ import com.google.inject.{Inject, Singleton}
 import models.{ParseResponseModel, ParseTreeViewModel}
 import play.api.data._
 import play.api.data.Forms._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import services.AntlrParser
 
 @Singleton
-class ParserController @Inject() (parser: AntlrParser) extends Controller {
+class ParserController @Inject() (parser: AntlrParser, val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   implicit val treeViewModel = Json.writes[ParseTreeViewModel]
   implicit val responseModel = Json.writes[ParseResponseModel]
@@ -17,23 +18,18 @@ class ParserController @Inject() (parser: AntlrParser) extends Controller {
   case class ParseRequest(src: String, grammar: String, rule: String)
 
   val form = Form(mapping(
-    "src" -> text,
-    "grammar" -> text,
+    "src" -> nonEmptyText,
+    "grammar" -> nonEmptyText,
     "rule" -> text
   )(ParseRequest.apply)(ParseRequest.unapply))
 
   def parseSrc() = Action { implicit request =>
+
     form.bindFromRequest.fold(
-      formWithErrors => BadRequest(formWithErrors.errors.map(_.message).mkString(", ")),
-      parsedForm => parsedForm match {
-        case ParseRequest(null, _, _) => BadRequest("src parameter must be specified, content should be form encoded")
-        case ParseRequest(_, null, _) => BadRequest("grammar parameter must be specified, content should be form encoded")
-        case ParseRequest(src, grammarSrc, rule) => {
-          parser.parse(grammarSrc, rule.trim, src) match {
-            case (Some(t), rules) => Ok(Json.toJson(ParseResponseModel(t, rules)))
-            case _ => BadRequest("There are errors in grammar, source cannot be parsed")
-          }
-        }
+      formWithErrors => BadRequest(formWithErrors.errorsAsJson),
+      parsedForm => parser.parse(parsedForm.grammar, parsedForm.rule.trim, parsedForm.src) match {
+        case (Some(t), rules) => Ok(Json.toJson(ParseResponseModel(t, rules)))
+        case _ => BadRequest("There are errors in grammar, source cannot be parsed")
       })
     }
 
