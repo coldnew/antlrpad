af6915bce75a948f1d33b36a55d282c1ecc04673
Alexey Nesterov <alexey.nesterov@live.com>
2016-08-13 22:58:43 +0100

    Update tests
    Include grammar warnings and errors check

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -13,7 +13,7 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest with AntlrFakeApp {
 
   "Routes" should {
 
-    "send 404 on a bad request" in  {
+    "send 404 on a non-existing paget" in  {
       route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
     }
 
@@ -36,15 +36,27 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest with AntlrFakeApp {
       status(home) mustBe BAD_REQUEST
     }
 
-    /* "return errors is incorrect" in {
+    "return grammar errors" in {
       val home = route(app, FakeRequest(POST, "/api/parse/").withFormUrlEncodedBody(
         ("grammar", "grammar test; \n two: '2'"),
         ("src", "2"),
         ("rule", "")
       )).get
 
-      status(home) mustBe BAD_REQUEST
-    } */
+      status(home) mustBe OK
+      contentAsString(home) must include("syntax error: mismatched input")
+    }
+
+    "return grammar warnings" in {
+      val req = route(app, FakeRequest(POST, "/api/parse/").withFormUrlEncodedBody(
+        ("grammar", "grammar test; \n id: ID; ID: 'a'*;"),
+        ("src", "2"),
+        ("rule", "")
+      )).get
+
+      status(req) mustBe OK
+      contentAsString(req) must include("non-fragment lexer rule ID can match the empty string")
+    }
 
     "parse the result " in {
       val home = route(app, FakeRequest(POST, "/api/parse/").withFormUrlEncodedBody(
