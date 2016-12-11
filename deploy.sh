bff6d717ed7daa5a82b6f16d1851c60217222e75
Alexey Nesterov <alexey.nesterov@live.com>
2016-12-11 17:17:20 +0000

    Fix deployment
    There is no point to use ansible for local builds

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i deploy/hosts deploy/deploy.yaml
+sbt universal:packageZipTarball && docker build -t aleksys/antlrpad . && docker push aleksys/antlrpad && ansible-playbook -i deploy/hosts deploy/deploy.yaml
